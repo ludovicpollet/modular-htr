a6502dc973797610e28e89f15b459bfb2b83a60a
ludovicpollet <ludovic.pollet@protonmail.com>
2025-12-02 14:02:06 +0100

    adapt according to lib updates and config for pretraining

--- scripts/train.py
+++ scripts/train.py
@@ -3,6 +3,7 @@ from datasets import load_from_disk
 from torch import optim
 
 from src.data.dataloaders import make_dataloaders
+from src.data.hf_dataset import build_hf_dataset
 from src.data.tokenization import apply_ctc_tokenizer, build_char_tokenizer
 from src.model.CRNN import CRNN
 from src.training.ctc import CTCLossWrapper
@@ -24,25 +25,42 @@ def main():
     device = get_device()
     print(f"Using device: {device}")
 
-    ds = load_from_disk("data/hfds")
+    # ds = load_from_disk("data/hfds")
+    # build_hf_dataset(
+    #     "/home/ludovic/Documents/proj/datasets/HOME-alcar/preproc_data/page",
+    #     "/home/ludovic/Documents/proj/datasets/HOME-alcar/preproc_data",
+    #     "data/home",
+    # )
+    ds = load_from_disk("data/home")
 
+    print("Building tokenizer...")
     tokenizer = build_char_tokenizer(ds, text_col="text")
+    print(f"Done. Tokenizer size: {len(tokenizer)}")
 
+    print("Applying tokenizer to dataset...")
     ds = apply_ctc_tokenizer(ds, tokenizer, text_col="text")
+    print("Done.")
 
-    dataset_name = "dg31"
+    dataset_name = "HOME-alcar"
 
-    fixed_height = 128
+    fixed_height = 64
     batch_size = 32
+    accum_steps = 1
+
+    pin_memory = device.type != "cpu"
+
+    print("Building dataloaders...")
 
     train_loader, val_loader = make_dataloaders(
         ds,
-        tokenizer=tokenizer,
         fixed_height=fixed_height,
         batch_size=batch_size,
-        num_workers=8,
+        num_workers=16,
         use_bucketing=True,
+        pin_memory=pin_memory,
     )
+
+    print("Done.")
 
     num_classes = len(tokenizer)
     img_channels = 1
@@ -60,11 +78,13 @@ def main():
         model = torch.compile(model)
 
     loss_fn = CTCLossWrapper(blank=tokenizer.blank_index)
-    epochs = 150
+    epochs = 80
     lr = 3e-4
     weight_decay = 1e-5
-    eta_min = 3e-5
-    optimizer = optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
+    eta_min = 2e-5
+    optimizer = optim.AdamW(
+        model.parameters(), lr=lr, weight_decay=weight_decay, fused=True
+    )
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
         optimizer, T_max=epochs, eta_min=eta_min
     )
@@ -76,6 +96,7 @@ def main():
         "val_samples": len(ds["test"]),
         "fixed_height": fixed_height,
         "batch_size": batch_size,
+        "accum_steps": accum_steps,
         "lr": lr,
         "weight_decay": weight_decay,
         "eta_min": eta_min,
@@ -88,7 +109,7 @@ def main():
         },
     }
 
-    run_dir = create_run_dir(base_dir="runs", run_name="test-checkpointing")
+    run_dir = create_run_dir(base_dir="runs", run_name="test-pretrain-optim")
     print(f"Run directory: {run_dir}")
 
     # for convenience (it goes in checkpoint anyway)
@@ -108,6 +129,7 @@ def main():
         train_loader=train_loader,
         val_loader=val_loader,
         optimizer=optimizer,
+        accum_steps=accum_steps,
         loss_fn=loss_fn,
         device=device,
         scaler=scaler,
